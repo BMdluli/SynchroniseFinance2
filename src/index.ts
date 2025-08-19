00f7ba43a4816408f601ba532c8264cd45ab5fcf
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-08-19 08:12:15 +0200

    update: added new domain to cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://sync-finance.vercel.app",
+    ],
     credentials: true,
   })
 );
